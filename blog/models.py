6ec7e10ae2d168b751671e1e3a27f0a71e24dd3d
Magesh Kannan <magkannan1@gmail.com>
2023-11-06 23:54:13 +0000

    Finish generic relationships

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,24 @@ from django.db import models
 from django.db import models
 from django.conf import settings
 
+##generic keys
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+
+# Declare before other models that this class uses
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    #post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
 class Tag(models.Model):
     value = models.TextField(max_length=100)
 
@@ -20,6 +38,10 @@ class Post(models.Model):
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
+
+
+
